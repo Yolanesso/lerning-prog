e58cea20d306c8d733fb2593fcff05292c956166
Станислав Лаптев <stas-lap@mail.ru>
2026-05-18 23:42:34 +0700

    написал метод сохраняюзий прогресс юзера в бд

--- backend/src/main/java/com/example/backend/service/LessonCompletionService.java
+++ backend/src/main/java/com/example/backend/service/LessonCompletionService.java
@@ -1,8 +1,14 @@
 package com.example.backend.service;
 
+import com.example.backend.dto.LessonCompletionDto;
+import com.example.backend.entity.LessonCompletion;
+import com.example.backend.entity.User;
 import com.example.backend.repository.LessonCompletionRepository;
 import com.example.backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class LessonCompletionService {
@@ -15,5 +21,20 @@ public class LessonCompletionService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
+    public void saveCompletionTask(String username, LessonCompletionDto dto) {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Пользователь не был найден"));
+        LessonCompletion completion = lessonCompletionRepository.findByUserAndLessonId(user, dto.getLessonId())
+                .orElse(new LessonCompletion());
+
+        completion.setUser(user);
+        completion.setLessonId(dto.getLessonId());
+        completion.setScore(dto.getScore());
+
+        if(completion.getId() != null) {
+            completion.setCompletedAt(LocalDateTime.now());
+        }
+        lessonCompletionRepository.save(completion);
+    }
 
 }
