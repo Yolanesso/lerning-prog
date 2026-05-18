78ea69a518def262290d08478d41290722ccc7e9
Станислав Лаптев <stas-lap@mail.ru>
2026-05-18 23:48:41 +0700

    написал метод в лессонКонтроллере

--- backend/src/main/java/com/example/backend/controller/LessonCompletionController.java
+++ backend/src/main/java/com/example/backend/controller/LessonCompletionController.java
@@ -1,7 +1,13 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.LessonCompletionDto;
 import com.example.backend.entity.LessonCompletion;
 import com.example.backend.service.LessonCompletionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,12 @@ public class LessonCompletionController {
         this.lessonCompletionService = lessonCompletionService;
     }
 
+    @PostMapping("/complete")
+    public ResponseEntity<String> completeLesson(@RequestBody LessonCompletionDto completionDto, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+
+        lessonCompletionService.saveCompletionTask(username,completionDto);
+
+        return ResponseEntity.ok("Прогресс урока сохранён!");
+    }
 }
